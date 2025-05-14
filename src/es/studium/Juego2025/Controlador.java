1ddfb1e440bee96852cb26c865e0eda7deb0d9d3
Jorgeeemilio <jorge@grupostudium.com>
2025-05-14 19:25:48 +0200

    New class Controlador

---
+++ src/es/studium/Juego2025/Controlador.java
@@ -0,0 +1,13 @@
+package es.studium.Juego2025;
+
+public class Controlador
+{
+	Vista vista;
+	Modelo modelo;
+	
+	public Controlador(Vista v, Modelo m)
+	{
+		this.vista = v;
+		this.modelo = m;
+	}
+}
